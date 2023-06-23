42c4453b16ee7ced2276570761791c0787b7c48d
Dariya <daria17belousova@gmail.com>
2023-06-23 14:31:49 +0300

    пятая задача

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number < 1000)   
+{
+int nam = (number % 10 - number % 1) / 1; 
+Console.WriteLine($"Третья цифра числа {number} является {nam}"); 
+}
+else
+Console.WriteLine("Это число не трехзначное");
